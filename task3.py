55c63026ba191b7f49fe91399cb52fa7250eb931
Youssef Abu-Hamda <shbanmhmdywsf8@gmail.com>
2023-01-01 10:35:04 +0200

    task3

---
+++ task3.py
@@ -0,0 +1,18 @@
+def balanced_parens(n):
+    ans = []
+    def generate(parens, opens, closes):
+        if opens == n and closes == n:
+            ans.append(parens)
+            return
+        elif opens == n:
+            ans.append(parens+")"*(n-closes))
+            return
+
+        if opens > closes:
+            generate(parens + ")", opens, closes + 1)
+        generate(parens + "(", opens+1, closes)
+
+    generate("", 0, 0)
+    return ans
+
+print(balanced_parens(3))
