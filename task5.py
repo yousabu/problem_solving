7df6933fbb793268bfdba8a45972254bac5173aa
Youssef Abu-Hamda <shbanmhmdywsf8@gmail.com>
2023-01-04 19:01:50 +0200

    task5

---
+++ task5.py
@@ -0,0 +1,19 @@
+def xo(s):
+    x = 0
+    o = 0
+    for i in s:
+        if i == 'o' or i == 'O':
+            o = o+1
+        elif i == 'x' or i == 'X':
+            x = x+1
+    if x == o:
+        return True
+
+    return False
+
+####################
+def xo(s):
+    s = s.lower()
+    return s.count('x') == s.count('o')
+
+print(xo("sdfsfsXxoo"))
