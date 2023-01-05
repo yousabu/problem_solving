7d7ee3017626ee1566f85b3354382c1739fb199b
Youssef Abu-Hamda <shbanmhmdywsf8@gmail.com>
2023-01-05 10:32:30 +0200

    task7

--- task7.py
+++ task7.py
@@ -1,9 +1,27 @@
 def digital_root(n):
     result = 0
-    for i in list(str(n)):
+    n_list = list(str(n))
+    for i in n_list:
         result = result+int(i)
+    if result > 9:
+        result = digital_root(result)
     return result
 
 
 
-print(digital_root(16))
+
+x = digital_root(942)
+
+print(x)
+
+
+# def digital_root(n):
+#     answ = 0
+#     s = 0
+#     x = str(n)
+#     for i in range(0, len(x)):
+#         s = s + int(x[i])
+#     if len(str(s)) > 1:
+#        s = digital_root(s)
+#     answ = s # You could even return s directly
+#     return answ
