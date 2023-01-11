f2be2ea85bf0d2026cf53e63458f8fe9d42ff386
Youssef Abu-Hamda <shbanmhmdywsf8@gmail.com>
2023-01-11 15:55:32 +0200

    edit

--- task2.sh
+++ task2.sh
@@ -10,3 +10,25 @@ for var in  {1..50}
 do
     echo $var;
 done
+
+func2 () {
+   if [ -z "$1" ]                           # Is parameter #1 zero length?
+   then
+     echo "-Parameter #1 is zero length.-"  # Or no parameter passed.
+   else
+     echo "-Param #1 is \"$1\".-"
+   fi
+
+   variable=${1-$DEFAULT}                   #  What does
+   echo "variable = $variable"              #+ parameter substitution show?
+                                            #  ---------------------------
+                                            #  It distinguishes between
+                                            #+ no param and a null param.
+
+   if [ "$2" ]
+   then
+     echo "-Parameter #2 is \"$2\".-"
+   fi
+
+   return 0
+}
