52ef1322bb4573cb7f58b27b691bc7080d2973cb
Youssef <shbanmhmdywsf8@gmail.com>
2023-01-24 17:43:55 +0200

    task5 sh

---
+++ task5.sh
@@ -0,0 +1,22 @@
+if [ $X -lt $Y ]; then
+  echo "X is less than Y"
+elif [ $X -gt $Y ]; then
+  echo "X is greater than Y"
+else
+  echo "X is equal to Y"
+fi
+
+
+read x
+read y 
+
+
+if [[ $x -gt  $y ]];
+then 
+    echo "X is greater than Y "
+elif [[ $x -eq $y ]];
+then
+    echo "X is equal to Y "
+else
+    echo "X is less than Y "
+fi
